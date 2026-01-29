bde75d9978c3b9f3a46191b5cb288e9830da2bf8
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-01-29 16:28:30 +0100

    TAS-23 feat(tasks): implement task update functionality

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -33,28 +33,27 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
-       
-        try{
-             $validated = $request->validate([
-            "title" => 'required|string|max:255',
-            "description" => 'nullable|string|max:255',
-            "priority" => 'required|in:low,medium,high',
-            "deadline" => 'nullable|date'
-            ]); 
+
+        try {
+            $validated = $request->validate([
+                "title" => 'required|string|max:255',
+                "description" => 'nullable|string|max:255',
+                "priority" => 'required|in:low,medium,high',
+                "deadline" => 'nullable|date'
+            ]);
 
             auth()->user()->tasks()->create([
                 "title" => $validated['title'],
-                "description" => $validated['description']?? null,
+                "description" => $validated['description'] ?? null,
                 "priority" =>  $validated['priority'],
-                "deadline" =>  $validated['deadline']?? null,
+                "deadline" =>  $validated['deadline'] ?? null,
                 "status" => 'todo' // default dyalo 
             ]);
 
             return redirect()
-            ->route('tasks')
-            ->with('success', 'Task created successfully');
-
-        }catch(\Exception $e){
+                ->route('tasks')
+                ->with('success', 'Task created successfully');
+        } catch (\Exception $e) {
             return back()
                 ->with("error", 'Failed to create task');
         }
@@ -64,5 +63,34 @@ class TaskController extends Controller
     {
         return view('tasks.edit', compact('task'));
     }
-}
 
+    public function update(Request $request, Task $task)
+    {
+        try {
+            $validated = $request->validate([
+                "title" => 'required|string|max:255',
+                "description" => 'nullable|string|max:255',
+                "priority" => 'required|in:low,medium,high',
+                "deadline" => 'nullable|date'
+            ]);
+
+            if ($task->user_id !== auth()->id()) {
+                abort(403);
+            }
+
+            $task->update([
+                "title" => $validated['title'],
+                "description" => $validated['description'] ?? null,
+                "priority" =>  $validated['priority'],
+                "deadline" =>  $validated['deadline'] ?? null,
+            ]);
+
+            return redirect()
+                ->route('tasks')
+                ->with('success', 'Task updated successfully');
+        } catch (\Exception $e) {
+            return back()
+                ->with("error", 'Failed to update task');
+        }
+    }
+}
